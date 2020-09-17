5e7e8dba6fb60e1736918b765195236f885d5db3
Zvolinskaya Katerina <62592754+katerina-kz@users.noreply.github.com>
2020-09-17 21:00:49 +0300

    starting 2ht

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
-import TableWork from "./components/TableWork";
+import TableWork from "./home-tasks/ht1/components/TableWork";
+import Blog from "./home-tasks/ht2/Blog";
+
 
 
   class App extends Component {
     render() {
       return (
         <TableWork />
+        // <Blog />
       )
     }
   }
